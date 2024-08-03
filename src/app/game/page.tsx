149016df93a2f82054b9be3c84158b687ac0d7e8
wqr1 <dimakond520@gmail.com>
2024-08-03 19:09:42 +0400

    Made CurrentKingdom component to display various kingdoms with the same screen size

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,24 @@
+import Coin from "@/assets/svg/game/coin.svg";
+import CurrentKingdom from "@/components/game/CurrentKingdom";
+
 const Game = () => {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <CurrentKingdom kingdomType="aqua" kingdomTier="second" />
+      <div className="flex flex-col gap-2">
+        <div className="mx-auto mt-[5%] flex size-fit items-center gap-[8px] border-[1px] px-[10px]">
+          <h6 className="font-normal text-[#B1FF82]">10 000 000</h6>
+          <Coin />
+        </div>
+        <div>
+          <p className="mx-auto flex size-fit gap-[10px] border-[1px] border-white px-[10px]">
+            <span className="">12 034</span>
+            <span>24H</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Game;
---
+++ src/components/game/CurrentKingdom.tsx
@@ -0,0 +1,64 @@
+import PlantKingdomTier1 from "@/assets/svg/game/kingdoms/plant/tier-1.svg";
+import PlantKingdomTier2 from "@/assets/svg/game/kingdoms/plant/tier-2.svg";
+import PlantKingdomTier3 from "@/assets/svg/game/kingdoms/plant/tier-3.svg";
+import PlantKingdomTier4 from "@/assets/svg/game/kingdoms/plant/tier-4.svg";
+
+import SunKingdomTier1 from "@/assets/svg/game/kingdoms/sun/tier-1.svg";
+import SunKingdomTier2 from "@/assets/svg/game/kingdoms/sun/tier-2.svg";
+import SunKingdomTier3 from "@/assets/svg/game/kingdoms/sun/tier-3.svg";
+import SunKingdomTier4 from "@/assets/svg/game/kingdoms/sun/tier-4.svg";
+
+import ElectroKingdomTier1 from "@/assets/svg/game/kingdoms/electro/tier-1.svg";
+import ElectroKingdomTier2 from "@/assets/svg/game/kingdoms/electro/tier-2.svg";
+import ElectroKingdomTier3 from "@/assets/svg/game/kingdoms/electro/tier-3.svg";
+import ElectroKingdomTier4 from "@/assets/svg/game/kingdoms/electro/tier-4.svg";
+
+import AquaKingdomTier1 from "@/assets/svg/game/kingdoms/aqua/tier-1.svg";
+import AquaKingdomTier2 from "@/assets/svg/game/kingdoms/aqua/tier-2.svg";
+import AquaKingdomTier3 from "@/assets/svg/game/kingdoms/aqua/tier-3.svg";
+import AquaKingdomTier4 from "@/assets/svg/game/kingdoms/aqua/tier-4.svg";
+
+const CurrentKingdom = ({
+  kingdomType,
+  kingdomTier,
+}: {
+  kingdomType: "sun" | "plant" | "electro" | "aqua";
+  kingdomTier: "first" | "second" | "third" | "fourth";
+}) => {
+  const kingdoms = {
+    plant: {
+      first: PlantKingdomTier1,
+      second: PlantKingdomTier2,
+      third: PlantKingdomTier3,
+      fourth: PlantKingdomTier4,
+    },
+    sun: {
+      first: SunKingdomTier1,
+      second: SunKingdomTier2,
+      third: SunKingdomTier3,
+      fourth: SunKingdomTier4,
+    },
+    electro: {
+      first: ElectroKingdomTier1,
+      second: ElectroKingdomTier2,
+      third: ElectroKingdomTier3,
+      fourth: ElectroKingdomTier4,
+    },
+    aqua: {
+      first: AquaKingdomTier1,
+      second: AquaKingdomTier2,
+      third: AquaKingdomTier3,
+      fourth: AquaKingdomTier4,
+    },
+  };
+
+  const Kingdom = kingdoms[kingdomType][kingdomTier];
+
+  return (
+    <div className="mt-[10%] flex h-[160px] w-full justify-center">
+      <Kingdom />
+    </div>
+  );
+};
+
+export default CurrentKingdom;
