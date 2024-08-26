211bc1ba1269ebd66388e0f54c3fa1474adfc980
wqr1 <dimakond520@gmail.com>
2024-08-26 20:46:08 +0400

    Debug #7

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -1,30 +1,20 @@
+import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const body: { initData: string } = await req.json();
+  const body: { initDataRaw: string } = await req.json();
 
-  const res = await fetch(`${process.env.API_URL}/api/v1/auth/login`, {
-    method: "POST",
-    body: JSON.stringify({
-      init_data: body.initData,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      "x-api-key": `${process.env.SECRET_API_KEY}`,
+  console.log(body.initDataRaw);
+
+  const res = await axios.post(
+    `${process.env.API_URL}/api/v1/auth/login`,
+    body.initDataRaw,
+    {
+      headers: {
+        "x-api-key": `${process.env.SECRET_API_KEY}`,
+      },
     },
-    cache: "no-store",
-  });
-
-  console.log(
-    JSON.stringify({
-      init_data: body.initData,
-    }),
   );
 
-  if (!res.ok) throw new Error("Error in /v1/auth/login");
-
-  const data = await res.json();
-
-  return NextResponse.json(data);
+  return NextResponse.json(res);
 }
