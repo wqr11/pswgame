796b07b6e6829101b3babb812a1ddd2981206063
Dmitry Kondratyev <dimakond520@gmail.com>
2024-09-02 22:21:58 +0400

    Auth debug

--- src/shared/api/login/login.ts
+++ src/shared/api/login/login.ts
@@ -3,28 +3,24 @@
 import axios, { isAxiosError } from 'axios';
 
 export async function login(init_data: string) {
-  // try {
-  const res = await axios.post(
-    `${process.env.API_URL}/api/v1/auth/login`,
-    { init_data },
-    {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+  try {
+    const res = await axios.post(
+      `${process.env.API_URL}/api/v1/auth/login`,
+      { init_data },
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
       },
-    },
-  );
+    );
 
-  // update cookies
+    console.log(res.data);
 
-  console.log(res);
-
-  return res;
-
-  //   return res;
-  // } catch (error) {
-  //   if (isAxiosError(error)) {
-  //     throw new Error(error.message);
-  //   }
-  // }
+    return res;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw new Error(error.message);
+    }
+  }
 }
