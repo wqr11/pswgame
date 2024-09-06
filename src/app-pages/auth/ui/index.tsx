1e9e1e882fdc91dd5108b504b263fc194f035b3c
wqr1 <dimakond520@gmail.com>
2024-09-06 23:27:33 +0400

    Fixed data path from api

--- src/app-pages/auth/ui/index.tsx
+++ src/app-pages/auth/ui/index.tsx
@@ -13,7 +13,7 @@ import { useInitData } from '@telegram-apps/sdk-react';
 import { setUserId } from '@/shared/entities/user';
 
 import { useUnit } from 'effector-react';
-import { authenticate, $auth } from '@/shared/entities/auth';
+import { login, $auth } from '@/shared/entities/auth';
 
 export const AuthPageUI = () => {
   const router = useRouter();
@@ -31,11 +31,10 @@ export const AuthPageUI = () => {
   }, [initData]);
 
   useEffect(() => {
-    authenticate(`${initDataRaw}`);
+    login(`${initDataRaw}`);
   }, [initDataRaw]);
 
   useEffect(() => {
-    console.log(auth);
     if (!!auth) {
       router.push('/game');
     }
--- src/shared/entities/auth/store.ts
+++ src/shared/entities/auth/store.ts
@@ -4,14 +4,14 @@ import axios from "axios";
 
 import Cookies from "js-cookie";
 
-import { createStore, createEffect, sample } from "effector";
+import { createStore, createEffect, sample, createEvent } from "effector";
 
 import { AuthDataType, TokensType } from "./types";
 
 import { getResources } from "../resources";
 import { getLeaders } from "../leaderboard";
 
-export const authenticate = createEffect(async (init_data: string) => {
+export const login = createEffect(async (init_data: string) => {
   const access = Cookies.get(`${process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME}`);
   const refresh = Cookies.get(`${process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME}`);
 
@@ -20,7 +20,7 @@ export const authenticate = createEffect(async (init_data: string) => {
   }
 
   try {
-    const res: AuthDataType = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/login`, {
+    const res: { data: AuthDataType } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/login`, {
       init_data: init_data
     }, {
       headers: {
@@ -29,18 +29,26 @@ export const authenticate = createEffect(async (init_data: string) => {
       }
     });
 
-    Cookies.set(`${process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME}`, res.data.access_token);
-    Cookies.set(`${process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME}`, res.data.refresh_token);
+    Cookies.set(`${process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME}`, res.data.data.access_token);
+    Cookies.set(`${process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME}`, res.data.data.refresh_token);
 
-    return { access: res.data.access_token, refresh: res.data.refresh_token } as TokensType;
+    return { access: res.data.data.access_token, refresh: res.data.data.refresh_token } as TokensType;
   } catch (error) {
     return null;
   }
 });
 
-export const $auth = createStore<TokensType | null>(null).on(authenticate.doneData, (_, auth) => auth);
+export const logout = createEffect<void, void, Error>(() => {
+  Cookies.remove(`${process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME}`);
+  Cookies.remove(`${process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME}`);
+  return;
+});
+
+export const $auth = createStore<TokensType | null>(null)
+  .on(login.doneData, (_, auth) => auth)
+  .reset(logout);
 
 sample({
-  clock: authenticate,
+  clock: login,
   target: [getResources, getLeaders]
 })
