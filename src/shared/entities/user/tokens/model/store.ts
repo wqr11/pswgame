e21afb4813f755a6618df3573b00850294b0414d
wqr1 <dimakond520@gmail.com>
2024-09-07 19:33:28 +0400

    Added coins on start

--- src/shared/entities/user/tokens/model/store.ts
+++ src/shared/entities/user/tokens/model/store.ts
@@ -1,14 +1,23 @@
 'use client';
 
-import { createEvent, createStore, sample } from "effector";
+import { combine, createEvent, createStore, sample } from "effector";
 
 import { $tap_multiplier, postTap, tap } from "@/shared/entities/tap";
+import { $user } from "../../user-data";
 
 export const setTokens = createEvent<number | undefined>();
+
+export const $initTokens = combine($user, (user) => user?.tokens_amount);
 
 export const $tokens = createStore<number>(0)
   .on(setTokens, (state, tokens) => tokens ?? state);
 
+
+sample({
+  clock: $user,
+  source: $initTokens,
+  target: setTokens,
+});
 
 sample({
   clock: tap,
