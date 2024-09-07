334bdb785c2a728549fe4e1ebcb3fc6f8f9fbba2
wqr1 <dimakond520@gmail.com>
2024-09-08 02:23:05 +0400

    Ping debug

--- src/shared/entities/ping/store.ts
+++ src/shared/entities/ping/store.ts
@@ -7,13 +7,13 @@ import { createEffect, createStore, sample } from "effector";
 import { $auth, TokensType, loggedIn, logout } from "../auth";
 import { PingDataType } from "./types";
 
-export const ping = createEffect<TokensType | null, PingDataType['data'] | undefined, AxiosError>(async (auth: TokensType | null) => {
+export const ping = createEffect<TokensType['access'], PingDataType['data'] | undefined, AxiosError>(async (access: TokensType['access']) => {
   try {
     const res: { data: PingDataType } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/users/ping`, {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'jwt-token': `${auth?.access}`
+        'jwt-token': `${access}`
       }
     })
 
@@ -25,28 +25,33 @@ export const ping = createEffect<TokensType | null, PingDataType['data'] | undef
   }
 });
 
-export const startPingInterval = createEffect<TokensType | null, number, Error>((auth) => {
-  return window.setInterval(() => {
-    ping(auth);
+export const startPingInterval = createEffect<TokensType['access'], NodeJS.Timeout, Error>((access: TokensType['access']) => {
+  return setInterval(() => {
+    ping(access);
   }, 1000); // 180 секунд
 });
 
-export const stopPingInterval = createEffect<number, void, Error>((intervalId: number) => {
-  window.clearInterval(intervalId);
+export const stopPingInterval = createEffect<NodeJS.Timeout, void, Error>((intervalId: NodeJS.Timeout) => {
+  clearInterval(intervalId);
 });
 
-export const $pingIntervalId = createStore<number>(0)
+export const $pingIntervalId = createStore<NodeJS.Timeout | null>(null)
   .on(startPingInterval.doneData, (_, intervalId) => intervalId)
+  .reset(stopPingInterval.doneData)
   .reset(logout);
 
 sample({
   clock: loggedIn,
-  source: $auth,
-  target: startPingInterval,
+  source: { auth: $auth },
+  filter: ({ auth }) => !!auth?.access,
+  //@ts-ignore
+  fn: ({ auth }) => ({ access: auth.access } as TokensType['access']),
+  target: ping, // change to startPingInterval
 });
 
 sample({
   clock: logout,
   source: $pingIntervalId,
+  filter: (pingIntervalId) => !!pingIntervalId,
   target: stopPingInterval,
 });
