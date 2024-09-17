a619c2b87385b4b27d4fda7548c308c61b3a7e50
wqr1 <dimakond520@gmail.com>
2024-09-17 23:00:06 +0400

    Ref scale

--- src/app/referral/layout.tsx
+++ src/app/referral/layout.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { AboutUs, RefTabSwitcher } from '@/widgets';
 import { useState, useEffect, type PropsWithChildren } from 'react';
-import styles from '@/app-pages/referral/styles/referral.module.css';
+
+import { AboutUs, RefTabSwitcher } from '@/widgets';
+
 import 'normalize.css';
 
 export default function ReferralLayout({ children }: PropsWithChildren) {
-  const [_, setScale] = useState(1);
+  const [scale, setScale] = useState(1);
   useEffect(() => {
     const updateScale = () => {
       const viewportHeight = window.innerHeight;
@@ -24,9 +25,10 @@ export default function ReferralLayout({ children }: PropsWithChildren) {
     return () => window.removeEventListener('resize', updateScale);
   }, []);
   return (
-    <main className="h-[100vh] w-[100vw] bg-[#0e0e0e] p-t-[29px]">
+    <main className="mx-auto h-screen w-screen overflow-clip">
       <div
-        className={`${styles.scroll} max-h-[800px] max-size-full relative z-30 flex size-full flex-col items-center overflow-x-clip text-center`}
+        className="relative z-30 flex size-full flex-col items-center text-center origin-top"
+        style={{ scale: scale }}
       >
         {children}
       </div>
