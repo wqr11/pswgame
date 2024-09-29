4fc8511c1af2a5a200c82b35433be825a13924f2
WQR1 <dimakond520@gmail.com>
2024-09-29 23:29:57 +0400

    Temporarily commented auto width for tokens and res displays

--- src/widgets/game/components/current-kingdom-display/ui/components/tokens-display/index.tsx
+++ src/widgets/game/components/current-kingdom-display/ui/components/tokens-display/index.tsx
@@ -14,9 +14,9 @@ export const TokensDisplay = () => {
     <div className="flex flex-col gap-2">
       <div
         className="mx-auto mt-8 flex items-center gap-[8px] border-[1px] px-[10px]"
-        style={{
-          width: `${(tokens.toString().length ?? 4) * 11}px`,
-        }}
+        // style={{
+        //   width: `${(tokens.toString().length ?? 4) * 11}px`,
+        // }}
       >
         <h6 className="font-normal">{formatNumber(tokens)}</h6>
         <Coin />
