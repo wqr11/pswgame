52bd172567ac41dcf5da61300ce993972824a070
WQR1 <dimakond520@gmail.com>
2024-10-30 23:24:24 +0400

    Fixing auth #3

--- src/entities/auth/store.ts
+++ src/entities/auth/store.ts
@@ -1,44 +1,23 @@
 'use client';
 
-import axios from 'axios';
-
-import Cookies from 'js-cookie';
-
 import { createStore, createEffect, sample, createEvent } from 'effector';
 
 import { AuthDataType } from './types';
+import { login } from '@/actions/auth/login';
 
 // get a pair of auth tokens
-export const loginFx = createEffect<string, boolean, Error>(async (init_data: string) => {
-  const accessToken = Cookies.get(`${process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME}`);
-  const refreshToken = Cookies.get(`${process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME}`);
-
-  if (accessToken && refreshToken) {
-    return true;
+export const loginFx = createEffect<string, AuthDataType['data'], Error>(
+  async (init_data: string) => {
+    return login(init_data);
   }
-
-  const res: { data: AuthDataType } = await axios.post(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/login`,
-    {
-      init_data,
-    },
-    {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    }
-  );
-
-  Cookies.set(`${process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME}`, res.data.data.access_token);
-  Cookies.set(`${process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME}`, res.data.data.refresh_token);
-
-  return true;
-});
+);
 
 export const loggedIn = createEvent<void>();
 
-export const $isAuth = createStore<boolean>(false).on(loginFx.doneData, (_, data) => data);
+export const $isAuth = createStore<boolean>(false).on(
+  loginFx.doneData,
+  (_, data) => data.access_token.length > 0 && data.refresh_token.length > 0
+);
 
 // fire loggedIn event on loginFx.doneData
 sample({
